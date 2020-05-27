83324421d4819c3dbd3595be010a5ddcf484dd16
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-05-27 17:49:57 -0500

    add GET and POST api/notes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-// app.use('/api', apiRoutes);
+app.use('/api/notes', apiRoutes);
 app.use('*', htmlRoutes);
 app.use('/notes', htmlRoutes);
 
