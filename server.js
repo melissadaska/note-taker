aeff2ac986b993bccccb163e6a7cb83e9b41a2ba
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-05-26 21:16:03 -0500

    add server.js and route files

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+
+app.listen(PORT, () => {
+    console.log('API server now on port' + PORT);
+})
+
