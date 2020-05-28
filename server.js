85b951338c35562a4fe9877b441817f460e3aa78
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-05-28 16:45:57 -0500

    add routes to server file & reorg code

--- server.js
+++ server.js
@@ -1,17 +1,70 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const path = require('path');
+const fs = require('fs');
 
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, './public')));
 
-app.use('/api/notes', apiRoutes);
-app.use('*', htmlRoutes);
-app.use('/notes', htmlRoutes);
+// initialize noteData
+var noteData = [];
+
+// API ROUTES
+
+// add GET route /api/notes to read db.json and return all saved notes as json
+app.get('/api/notes', (req, res) => {
+    try {
+        // read noteData from json file
+        noteData = fs.readFileSync('./db/db.json', 'utf8');
+        // parse noteData so is an array of objects
+        noteData = JSON.parse(noteData);
+    } catch (err) {
+        console.log(err);
+    }
+    // send noteData as json to the server
+    res.json(noteData);
+});
+
+// add POST route /api/notes to receive a new note to save on the request body, add to db.json file, and return the new note to the client
+app.post('/api/notes', (req, res) => {
+    try {
+        // read noteData from json file
+        noteData = fs.readFileSync('./db/db.json', 'utf8');
+        // parse data to get array of objects
+        noteData = JSON.parse(noteData);
+        // set new note id
+        req.body.id = noteData.length;
+        // push new note to array of note objects
+        noteData.push(req.body);
+
+        noteData = JSON.stringify(noteData);
+        // write new note to json file
+        fs.writeFile('./db/db.json', noteData, 'utf8', (err) => {
+            if (err) throw err;
+        });
+    } catch (err) {
+        throw err;
+    }
+});
+
+// ADD DELETE NOTE HERE
+
+// HTM ROUTES
+
+// FIGURE OUT WHY THE ORDER OF THESE MATTER!!!
+
+// add route "*" to return index.html file (homepage)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// add route /notes to return notes.html file when get started is clicked
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
 
 app.listen(PORT, () => {
